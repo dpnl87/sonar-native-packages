d28ffe57e71075d3aaadc396e3faf066052de9f5
Evgeny Mandrikov <mandrikov@gmail.com>
2015-08-15 13:23:58 +0200

    Travis: build both rpm and deb packages at the same time

--- travis.sh
+++ travis.sh
@@ -2,8 +2,15 @@
 
 set -euo pipefail
 
+wget http://downloads.sonarsource.com/sonarqube/sonarqube-5.1.2.zip --output-document=sonarqube-5.1.2.zip
+
+cp sonarqube-5.1.2.zip rpm/SOURCES/
 cd rpm
+./build.sh 5.1.2
 
-wget http://downloads.sonarsource.com/sonarqube/sonarqube-5.1.2.zip --output-document=SOURCES/sonarqube-5.1.2.zip
+cd ..
 
+cp sonarqube-5.1.2.zip deb/
+cd deb
+mkdir tmp
 ./build.sh 5.1.2
