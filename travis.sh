89cb835e2c8c7451379199f551cb1f2910c00c7b
Evgeny Mandrikov <mandrikov@gmail.com>
2015-08-15 13:11:13 +0200

    Travis: fix downloading of official distribution

--- travis.sh
+++ travis.sh
@@ -6,6 +6,6 @@ sudo apt-get install rpm
 
 cd rpm
 
-wget http://downloads.sonarsource.com/sonarqube/sonarqube-5.1.2.zip --output-document=SOURCES/sonar-5.1.2.zip
+wget http://downloads.sonarsource.com/sonarqube/sonarqube-5.1.2.zip --output-document=SOURCES/sonarqube-5.1.2.zip
 
 ./build.sh 5.1.2
