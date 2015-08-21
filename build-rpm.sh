1a6acfab7c403802b91dd0c3929fe18dee58e376
Daniel Paulus <dpaulus@Aura.local>
2015-08-21 15:44:21 +0200

    fetch the source zip if not already downloaded

--- build-rpm.sh
+++ build-rpm.sh
@@ -12,6 +12,11 @@ fi
 
 VER=$1
 
+if ! [[ -f /usr/distfiles/sonarqube-${VER}.zip ]]; then
+  echo "Download source with build version"
+  wget -P /usr/distfiles/ http://downloads.sonarsource.com/sonarqube/sonarqube-${VER}.zip
+fi
+
 echo "Building RPM package"
 rm -v rpm/SOURCES/*.zip || true
 cp -v /usr/distfiles/sonarqube-${VER}.zip rpm/SOURCES/
